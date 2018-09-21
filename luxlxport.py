c9cf3a782f9273f34ac3768977f2479a69e3fe33
joffa <Joffa@kth.se>
2018-09-22 01:04:30 +0200

    Lagt till en samlingsfil

--- luxlxport.py
+++ luxlxport.py
@@ -14,9 +14,13 @@ def main():
     """
     Här körs själva rutinen
     """
-    base_folder = u'D:\\Users\\Joffa\\PycharmProjects\\luxlxport\\data'  # Ändra till var excelfilerna ligger
-    out_folder = u'D:\\Users\\Joffa\\PycharmProjects\\luxlxport\\data'  # Ändra till var du vill lägga .csv-filerna
+    # base_folder = u'D:\\Users\\Joffa\\PycharmProjects\\luxlxport\\data'  # Ändra till var excelfilerna ligger
+    # out_folder = u'D:\\Users\\Joffa\\PycharmProjects\\luxlxport\\data'  # Ändra till var du vill lägga .csv-filerna
+    base_folder = u'/tmp'
+    out_folder = u'/tmp'
     extension = u'.xlsx'
+
+    total = None
 
     for root, dirnames, filenames in os.walk(base_folder):
         for filename in fnmatch.filter(filenames, u'*{}'.format(extension)):
@@ -29,6 +33,21 @@ def main():
             csv_filename = os.path.join(out_folder, u'{}.csv'.format(filename_wo_ext))
             df.to_csv(csv_filename, encoding=u'utf8', index=False)
 
+            # Lägg in GPDR-ID i en egen kolumn
+            df[u'GDPR-ID'] = df[df[u'Uppgift'] == u'GDPR-ID'][u'Svar'].values[0]
+
+            # Lägg ihop allt till en stor tabell
+            if total is None:
+                total = df
+            else:
+                total = total.append(df, ignore_index=True)
+
+    if total is not None:
+        total_csv_filename = os.path.join(out_folder, u'totala.csv')
+        total.to_csv(total_csv_filename, encoding=u'utf8', index=False)
+
+    return total
+
 
 if __name__ == '__main__':
-    main()
+    df = main()
